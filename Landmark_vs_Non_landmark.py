a01cb58a30650a636c3c27888b9639dfdf6cae8a
shloksethia-6119 <ss6119@columbia.edu>
2021-06-12 22:22:33 +0530

    separate landmark from non landmark

---
+++ Landmark_vs_Non_landmark.py
@@ -0,0 +1,16 @@
+import timm
+import pandas as pd
+import warnings
+from timm.models.efficientnet import efficientnet_l2
+
+from timm.models.resnest import resnest269e, resnest50d_4s2x40d
+from timm.models.resnet import resnext50_32x4d
+warnings.filterwarnings('ignore')
+
+import os
+import sys
+import glob
+import sys
+from pathlib import Path
+root_path = Path(os.getcwd())
+sys.path.append(str(root_path.parent))
